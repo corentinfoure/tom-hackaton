75380269abb2e0417eb2cda0e2b49acedffa9bfe
Nathan Fradin <nathan.fradin@doctolib.com>
2024-07-08 16:16:22 +0200

    colors

---
+++ components/style/colors.ts
@@ -0,0 +1,32 @@
+
+export const theme = {
+    background: 'white',
+    statusbar: {
+        background: 'white',
+        color: 'blue',
+    },
+    text: {
+        primary: 'black',
+        secondary: 'white',
+    },
+    button: {
+        primary: {
+            background: 'lightblue',
+            border: 'black',
+        },
+        secondary: {
+            background: 'white',
+            border: 'black',
+        },
+    },
+    inputField: {
+        primary: {
+            background: 'white',
+            border: 'black',
+        },
+        secondary: {
+            background: 'lightgrey',
+            border: 'black',
+        },
+    },
+}
