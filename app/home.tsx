09c302ea12219c5d34490a999b58d9c971442441
Nathan Fradin <nathan.fradin@doctolib.com>
2024-07-08 16:42:49 +0200

    info banner

--- app/home.tsx
+++ app/home.tsx
@@ -1,6 +1,7 @@
 import { RootRouteParams } from "@/App";
 import type { ItemData } from "@/components/shared/Container3Layout";
 import { Container3Layout } from "@/components/shared/Container3Layout";
+import { InformationBanner } from "@/components/shared/InformationBanner";
 import { ThemedText } from "@/components/style/ThemedText";
 import { useEffect } from "react";
 import { Pressable, SafeAreaView, StyleSheet, View } from "react-native";
@@ -10,18 +11,15 @@ type HomeProps = RootRouteParams<"home">;
 export const Home: React.FC<HomeProps> = ({ navigation }) => {
   const items: ItemData[] = [
     {
-      title: "Préparer une prise de parole",
-      image: require("@/assets/images/react-logo.png"),
+      image: require("@/assets/images/ConseilCard.png"),
       onPress: () => navigation.navigate("speechCreate"),
     },
     {
-      title: "Mes conseils",
-      image: require("@/assets/images/react-logo.png"),
+      image: require("@/assets/images/PrepareCard.png"),
       onPress: () => navigation.navigate("advices"),
     },
     {
-      title: "Mes prises de parole",
-      image: require("@/assets/images/react-logo.png"),
+      image: require("@/assets/images/ConseilCard.png"),
       onPress: () => navigation.navigate("speechPreps"),
     },
   ];
@@ -41,7 +39,10 @@ export const Home: React.FC<HomeProps> = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.titleContainer}>
-      <Container3Layout items={items} />
+      <ThemedText type='subtitle'>Que dois-je faire aujourd'hui ?</ThemedText>
+      {/* <Container3Layout items={items} /> */}
+      <InformationBanner title="TITLE" description="nuifhiohfoghfofhgofhofhfophfoh
+      bfdigdigfigf" />
     </SafeAreaView>
   );
 };
